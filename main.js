1b9391a36a8ada2db51e83310490adb3e3e1d188
Sarah Jones <sarahrjones8@gmail.com>
2023-03-25 12:18:01 -0400

    complete lab

--- main.js
+++ main.js
@@ -2,8 +2,45 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
+//Grab all of the hearts
+const hearts = document.getElementsByClassName("like-glyph")
 
+//Run a loop to iterate over all of the hearts.
+for (const heart of hearts) {
+  heart.addEventListener("click", function() {
+    
+    //If the heart is empty when clicked, have it fill red.
+    if (heart.textContent === EMPTY_HEART) {
+      mimicServerCall()
+      .then(function () {
+        console.log("successful response")
+        heart.textContent = "♥"
+        heart.className = "activated-heart"
+      })
+
+      //Grab the modal. Show the modal and an error message if fetch returns an error.
+      .catch(function () {
+        const modal = document.getElementById("modal")
+        console.log("error message")
+        modal.className = "show"
+        const modalMessage = document.getElementById("modal-message")
+        modalMessage.textContent = "Random server error. Try again."
+
+        //Hide the modal 3 seconds after firing.
+        setTimeout(function() {
+          const modal = document.getElementById("modal")
+          console.log("error message")
+          modal.className = "hidden"
+        }, 3000)
+      })
+
+    //If the heart is full and red when clicked, have it empty.
+    } else if (heart.textContent === FULL_HEART) {
+        heart.textContent = '♡'
+        heart.className = "like-glyph"
+    }
+  })
+}
 
 
 
